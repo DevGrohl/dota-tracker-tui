32adf281853cd2ac00fb0e0435299fc90e7e6258
Miguel Lares <c.code.jp@gmail.com>
2024-08-29 23:57:41 -0600

    Call to save recent game

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/DevGrohl/dota-tracker-tui/internal/data"
+	"github.com/DevGrohl/dota-tracker-tui/pkg/matchhistory"
 	"github.com/joho/godotenv"
 )
 
@@ -16,13 +20,29 @@ func init() {
 func main() {
 	// Get match history
 	matchHistory := getMatchHistory(&optionalMatchHistory{matches_requested: "1"})
-	fmt.Printf("%+v\n", matchHistory)
+	fmt.Printf("%+v\n", matchHistory.Result.Matches[0])
 
 	// Access to data from defined user
-
-	// Get match history
+	getMatchDetails(matchHistory.Result.Matches[0].MatchID)
 
 	// Store relevant information
+	db := data.New()
+	if err := db.DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	var mh matchhistory.MatchHistory
+	mh.MatchID = matchHistory.Result.Matches[0].MatchID
+	mh.StartTime = time.Unix(int64(matchHistory.Result.Matches[0].StartTime), 0) // matchHistory.Result.Matches[0].StartTime
+	mh.LobbyType = matchHistory.Result.Matches[0].LobbyType
+
+	repo := data.MatchHistoryRepository{Data: db}
+	err := repo.Create(&mh)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data.Close()
 
 	// Display data in TUI
 }
